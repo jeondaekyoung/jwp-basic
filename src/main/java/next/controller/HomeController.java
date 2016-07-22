d68810242d04b0baaf769acf26b606396bf8d7a1
sjw <sjw@sjw-PC>
2016-07-22 11:51:34 +0900

    문제마다 주석 달기 / 3번문제 수정

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -3,15 +3,20 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import next.dao.QuestionDao;
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
+import next.model.User;
 
 public class HomeController extends AbstractController {
     private QuestionDao questionDao = new QuestionDao();
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return jspView("index.jsp").addObject("questions", questionDao.findAll());
+    	//테스트아이디
+    	User testUser = new User("jdk", "jdk", "전대경", "jdk@naver.com");
+        request.getSession().setAttribute("user", testUser);
+        
+    	return jspView("index.jsp").addObject("questions", questionDao.findAll());
     }
 }
--- src/main/java/next/controller/qna/CreateQnAController.java
+++ src/main/java/next/controller/qna/CreateQnAController.java
@@ -1,5 +1,8 @@
 package next.controller.qna;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,21 +22,25 @@ public class CreateQnAController extends AbstractController {
 	private static final Logger log = LoggerFactory.getLogger(CreateQnAController.class);
 
     private QuestionDao questionDao = new QuestionDao();
+    private long lastQuestionId ;
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//4번문제
 		if(request.getSession().getAttribute("user")==null){
 			 throw new NullPointerException("로그인 이후 이용하세요.");
-	
-			
 		}
+		//3번문제
+		//마지막 questionId 얻기
+		lastQuestionId=questionDao.findAll().get(0).getQuestionId();
+		log.debug("User : {}", lastQuestionId);
+		Question question = new Question(lastQuestionId+1,
+				request.getParameter("writer")
+				,request.getParameter("title"), 
+				request.getParameter("contents"),new Date(), 0);
 		
-		Question question = new Question(
-				request.getParameter("writer"), 
-				request.getParameter("title"), 
-				request.getParameter("contents")
-			);
 		
         log.debug("User : {}", question);
+        
         questionDao.insert(question);
 		return jspView("redirect:/");
 	}
